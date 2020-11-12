8c23b5d6a0a4f30b361c6ad6caee31e056e127a9
alexandros-cat <alexandros.cat@gmail.com>
2020-11-12 19:45:51 +0900

    postsコントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :home, only: :index
+  resources :home, only: :index do
+    resources :posts
+  end
+
 end
