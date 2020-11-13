047ca248cff59f2fd50c624d3379d00b5dfecc6e
alexandros-cat <alexandros.cat@gmail.com>
2020-11-13 23:09:13 +0900

    posts コントローラー　params 追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,18 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(post_params)
-    if@post.valid? 
-      @post.save
-       redirect_to root_path
+      post = Post.new(post_params)
+    if post.valid? 
+       post.save
+       redirect_to home_posts_path
     else
        render 'new'
     end  
   end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id,:movie)
+                                 .merge(user_id: current_user.id)
+  end
+
 end
