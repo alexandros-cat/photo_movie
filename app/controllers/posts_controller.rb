e8f621caa3839d01f16727f7159be97a18bc6318
alexandros-cat <alexandros.cat@gmail.com>
2020-11-13 12:14:55 +0900

    postsコントローラーnew/createアクション設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,18 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
   
+  def create
+    @post = Post.new(post_params)
+    if@post.valid? 
+      @post.save
+       redirect_to root_path
+    else
+       render 'new'
+    end  
+  end
 end
