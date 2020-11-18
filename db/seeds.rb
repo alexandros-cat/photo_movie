9f8e177471a7bffe7e100d911549f4294d7b8bd1
alexandros-cat <alexandros.cat@gmail.com>
2020-11-18 11:35:00 +0900

    Gem ランサック追加

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Category.create(name:'--')
+Category.create(name:'Forest')
+Category.create(name:'Town')
+Category.create(name:'Sea')
+Category.create(name:'Field')
+Category.create(name:'Mountain')
+Category.create(name:'Star')
+Category.create(name:'Other')
+
