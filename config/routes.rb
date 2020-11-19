9f8653687401e311db17814d894f3c5089587738
alexandros-cat <alexandros.cat@gmail.com>
2020-11-19 20:43:23 +0900

    My page 作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   root to: "home#index"
   resources :concept, only: :index
+  resources :favorites, only: :index
   resources :posts do 
     resources :comments, only: :create
     collection do
