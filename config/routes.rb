84efc60850d2c9d05b56b02d712cd95df0505f6a
alexandros-cat <alexandros.cat@gmail.com>
2020-11-29 13:14:14 +0900

    デプロイエラー対応

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   resources :users
   resources :concept, only: :index
   resources :posts do 
-    get 'posts/:id', to: 'posts#checked'
+    # get 'posts/:id', to: 'posts#checked'
     resources :comments, only: :create
     resource :favorites, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
+    end
+    member do
+      get 'checked'
     end
   end
   post 'like/:id' => 'likes#create', as: 'create_like'
