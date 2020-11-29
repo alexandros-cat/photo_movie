e328cb4f72fe6b6a29863181e9bbfc80e3093477
alexandros-cat <alexandros.cat@gmail.com>
2020-11-29 13:29:42 +0900

    デプロイエラー対応

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,30 +1,30 @@
-function check() {
-  const posts = document.querySelectorAll(".post");
-  posts.forEach(function (post) { 
-    if (post.getAttribute("data-load") != null) {
-      return null;
-    }
-    post.setAttribute("data-load", "true");
-    post.addEventListener("click", () => {  
-      const postId = post.getAttribute("data-id");
-      const XHR = new XMLHttpRequest();
-      XHR.open("GET", `/posts/${postId}`, true);
-      XHR.responseType = "json";
-      XHR.send();
-      XHR.onload = () => {
-        if (XHR.status != 200) {
-          alert(`Error ${XHR.status}: ${XHR.statusText}`);
-          return null;          
-        }
-        const item = XHR.response.post;
-        if (post.checked === true) {
-          post.setAttribute("data-check", "true");
-        } else if (post.checked === false) {
-          post.removeAttribute("data-check");
-        }
-      };
-     });
-  });
+// function check() {
+//   const posts = document.querySelectorAll(".post");
+//   posts.forEach(function (post) { 
+//     if (post.getAttribute("data-load") != null) {
+//       return null;
+//     }
+//     post.setAttribute("data-load", "true");
+//     post.addEventListener("click", () => {  
+//       const postId = post.getAttribute("data-id");
+//       const XHR = new XMLHttpRequest();
+//       XHR.open("GET", `/posts/${postId}`, true);
+//       XHR.responseType = "json";
+//       XHR.send();
+//       XHR.onload = () => {
+//         if (XHR.status != 200) {
+//           alert(`Error ${XHR.status}: ${XHR.statusText}`);
+//           return null;          
+//         }
+//         const item = XHR.response.post;
+//         if (post.checked === true) {
+//           post.setAttribute("data-check", "true");
+//         } else if (post.checked === false) {
+//           post.removeAttribute("data-check");
+//         }
+//       };
+//      });
+//   });
    
-}
-setInterval(check, 1000);
+// }
+// setInterval(check, 1000);
